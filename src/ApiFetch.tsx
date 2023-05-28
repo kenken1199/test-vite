824ca5a82865c9777bb954d3218cc455dd58f12b
kenken1199 <nakamori.k@gmail.com>
2023-05-28 21:28:01 +0900

    first commit

---
+++ src/ApiFetch.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+type SomeExample = {
+  id: number;
+  title: string;
+};
+
+const ApiFetch = () => {
+  const [posts, setPosts] = useState<SomeExample>({ id: 0, title: "" });
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+  return (
+    <div>
+      <ul>
+        <li>{posts.title}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ApiFetch;
